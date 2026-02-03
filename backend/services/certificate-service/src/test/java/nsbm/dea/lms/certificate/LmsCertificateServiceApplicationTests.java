9a174b747ce8d27bbd4a1e6c1fcb261d09e931bb
fazlaFiyas <fazlafiyas20011220@gmail.com>
2026-02-03 23:30:42 +0530

    certificate service added

---
+++ backend/services/certificate-service/src/test/java/nsbm/dea/lms/certificate/LmsCertificateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nsbm.dea.lms.certificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsCertificateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
