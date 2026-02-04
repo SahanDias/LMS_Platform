59e2de6c2fe1ff9d1df0e694d71f6ab61c44b620
fazlaFiyas <fazlafiyas20011220@gmail.com>
2026-02-04 05:59:15 +0530

    updated certificate service added

---
+++ backend/services/certificate-service/src/main/java/nsbm/dea/lms/certificate/LmsCertificateServiceApplication.java
@@ -0,0 +1,13 @@
+package nsbm.dea.lms.certificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsCertificateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsCertificateServiceApplication.class, args);
+	}
+
+}
