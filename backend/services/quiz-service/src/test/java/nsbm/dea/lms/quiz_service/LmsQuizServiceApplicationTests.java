7ef8cf25738336caebbed5db560193b4af8e14bd
Pavani Nisansala <lapnisansala@students.nsbm.ac.lk>
2026-02-21 14:38:12 +0530

    Set up initial Spring Boot quiz service

---
+++ backend/services/quiz-service/src/test/java/nsbm/dea/lms/quiz_service/LmsQuizServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nsbm.dea.lms.quiz_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsQuizServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
