b601516cb51477732eb367ba79d44df27b77c5e4
Hashiniwickramaarachchi <135943121+Hashiniwickramaarachchi@users.noreply.github.com>
2026-02-21 12:47:51 +0530

    Initialize notification branch and service project
    Created a new branch and set up the initial structure for my assigned work.

---
+++ backend/services/notification-service/src/main/java/nsbm/dea/lms/notification/LmsCourseServiceApplication.java
@@ -0,0 +1,13 @@
+package nsbm.dea.lms.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsCourseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsCourseServiceApplication.class, args);
+	}
+
+}
