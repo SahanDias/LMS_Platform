fd7e0cfbc80b9f4f4bc2042e960213b5354e4263
dnawarathna <dcdnawarathna@students.nsbm.ac.lk>
2026-02-21 14:15:26 +0530

    project added to new branch

---
+++ backend/services/progress-service/src/main/java/nsbm/dea/lms/progress_service/LmsProgressServiceApplication.java
@@ -0,0 +1,13 @@
+package nsbm.dea.lms.progress_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsProgressServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsProgressServiceApplication.class, args);
+	}
+
+}
---
+++ backend/services/progress-service/src/test/java/nsbm/dea/lms/progress_service/LmsProgressServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nsbm.dea.lms.progress_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsProgressServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
