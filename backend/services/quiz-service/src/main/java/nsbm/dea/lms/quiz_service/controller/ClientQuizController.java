81d226773a0e3411b13702e5ab60e59efd18ac9f
Pavani Nisansala <lapnisansala@students.nsbm.ac.lk>
2026-03-04 18:38:45 +0530

    Add client submit quiz endpoint

--- backend/services/quiz-service/src/main/java/nsbm/dea/lms/quiz_service/controller/ClientQuizController.java
+++ backend/services/quiz-service/src/main/java/nsbm/dea/lms/quiz_service/controller/ClientQuizController.java
@@ -2,6 +2,8 @@ package nsbm.dea.lms.quiz_service.controller;
 
 import nsbm.dea.lms.quiz_service.dto.QuizSummaryResponse;
 import nsbm.dea.lms.quiz_service.dto.StartQuizResponse;
+import nsbm.dea.lms.quiz_service.dto.SubmitQuizRequest;
+import nsbm.dea.lms.quiz_service.dto.SubmitQuizResponse;
 import nsbm.dea.lms.quiz_service.service.ClientQuizService;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,7 @@ import java.util.List;
   Student can:
   1) See available ACTIVE quizzes under a class
   2) Start a quiz and view questions + answers
+  3) Submit quiz attempt and get result
 */
 
 @RestController
@@ -45,5 +48,16 @@ public class ClientQuizController {
     @GetMapping("/{quizId}/start")
     public StartQuizResponse startQuiz(@PathVariable Long quizId) {
         return clientQuizService.startQuiz(quizId);
+    }
+
+    /*
+     POST http://localhost:8085/api/client/quizzes/{quizId}/submit
+     SubmitQuizRequest JSON
+   */
+
+    @PostMapping("/{quizId}/submit")
+    public SubmitQuizResponse submitQuiz(@PathVariable Long quizId,
+                                         @RequestBody SubmitQuizRequest request) {
+        return clientQuizService.submitQuiz(quizId, request);
     }
 }
