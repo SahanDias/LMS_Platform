e8776b6234c96d9720c6cc19875caa02e5f9709e
Udith Kavishan <ukhettiarachchi@students.nsbm.ac.lk>
2026-03-06 19:08:52 +0530

    add exception handling and  docker

--- backend/services/course-service/src/main/java/nsbm/dea/lms/course/config/CorsConfig.java
+++ backend/services/course-service/src/main/java/nsbm/dea/lms/course/config/CorsConfig.java
@@ -15,9 +15,11 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
+                                "http://localhost:3001",
                                 "http://localhost:8080",
                                 "http://localhost:8081",
                                 "http://localhost:8082",
+                                "http://127.0.0.1:3001",
                                 "http://127.0.0.1:8080",
                                 "http://127.0.0.1:8081",
                                 "http://127.0.0.1:8082"
