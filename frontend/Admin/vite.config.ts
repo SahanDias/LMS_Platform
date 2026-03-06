c7f573693fb881e3d4e76aa8e8e0892b6082ae63
nvppiyumika <pasindupiyumika4@gmail.com>
2026-03-07 01:37:09 +0530

    video service frontend added

--- frontend/Admin/vite.config.ts
+++ frontend/Admin/vite.config.ts
@@ -35,6 +35,10 @@ export default defineConfig(({ mode }) => ({
         target: "http://localhost:8080",
         changeOrigin: true,
       },
+      "/videos": {
+        target: "http://localhost:8083",
+        changeOrigin: true,
+      },
     },
   },
   plugins: [react()],
