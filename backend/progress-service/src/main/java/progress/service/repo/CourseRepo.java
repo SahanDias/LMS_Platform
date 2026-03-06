46fd6b4e1fce5ba36abb2115dd3a03e5030456d8
dnawarathna <dcdnawarathna@students.nsbm.ac.lk>
2026-03-07 04:31:06 +0530

    Revert "backend development"
    This reverts commit a936569108cef448d422f9be3d8de6671b1723fb.

--- backend/progress-service/src/main/java/progress/service/repo/CourseRepo.java
+++
@@ -1,8 +0,0 @@
-package progress.service.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import progress.service.entity.Course;
-
-public interface CourseRepo extends JpaRepository<Course,Integer> {
-
-}
--- backend/progress-service/src/main/java/progress/service/repo/QuizRepo.java
+++
@@ -1,7 +0,0 @@
-package progress.service.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import progress.service.entity.Quiz;
-
-public interface QuizRepo extends JpaRepository<Quiz ,Integer> {
-}
