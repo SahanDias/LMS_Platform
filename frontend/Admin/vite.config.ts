d826704afc063048fa42fad7195bee517ab8831b
Rasan Samarakkody <rasansamarakkody077@gmail.com>
2026-03-07 11:56:39 +0530

    vite port change

--- frontend/Admin/vite.config.ts
+++ frontend/Admin/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => ({
         changeOrigin: true,
       },
       "/class-api": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8081",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/class-api/, "/api"),
       },
